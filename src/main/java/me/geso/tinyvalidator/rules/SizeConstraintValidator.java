77b0f88105c452cfb065d035b00911fc237039cf
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-11-18 13:24:09 +0900

    Support surrogate pair.

--- src/main/java/me/geso/tinyvalidator/rules/SizeConstraintValidator.java
+++ src/main/java/me/geso/tinyvalidator/rules/SizeConstraintValidator.java
@@ -1,67 +1,71 @@
 package me.geso.tinyvalidator.rules;
 
-import me.geso.tinyvalidator.ConstraintValidator;
-import me.geso.tinyvalidator.constraints.Size;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.lang.annotation.Annotation;
 import java.util.Collection;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import me.geso.tinyvalidator.ConstraintValidator;
+import me.geso.tinyvalidator.constraints.Size;
+
 public class SizeConstraintValidator implements ConstraintValidator {
-    private static final Logger logger = LoggerFactory.getLogger(SizeConstraintValidator.class);
+	private static final Logger logger = LoggerFactory.getLogger(SizeConstraintValidator.class);
 
-    @Override
-    public boolean isValid(Annotation annotation,
-                           Object fieldValue) {
-        Size sizeAnnotation = (Size) annotation;
-        int size = getSize(fieldValue);
+	@Override
+	public boolean isValid(Annotation annotation,
+			Object fieldValue) {
+		Size sizeAnnotation = (Size)annotation;
+		int size = getSize(fieldValue);
 
-        if (size > sizeAnnotation.max()) {
-            if (logger.isDebugEnabled()) {
-                logger.debug("SizeConstraintValidator: {} > {}", size, sizeAnnotation.max());
-            }
-            return false;
-        } else if (size < sizeAnnotation.min()) {
-            if (logger.isDebugEnabled()) {
-                logger.debug("SizeConstraintValidator: {} < {}", size, sizeAnnotation.min());
-            }
-            return false;
-        }
-        return true;
-    }
+		if (size > sizeAnnotation.max()) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("SizeConstraintValidator: {} > {}", size, sizeAnnotation.max());
+			}
+			return false;
+		} else if (size < sizeAnnotation.min()) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("SizeConstraintValidator: {} < {}", size, sizeAnnotation.min());
+			}
+			return false;
+		}
+		return true;
+	}
 
-    private int getSize(Object fieldValue) {
-        if (fieldValue instanceof CharSequence) {
-            return ((CharSequence) fieldValue).length();
-        } else if (fieldValue instanceof Collection) {
-            return ((Collection<?>) fieldValue).size();
-        } else if (fieldValue instanceof Map) {
-            return ((Map<?, ?>) fieldValue).size();
-        } else if (fieldValue instanceof Object[]) {
-            return ((Object[]) fieldValue).length;
-        } else if (fieldValue instanceof boolean[]) {
-            return ((boolean[]) fieldValue).length;
-        } else if (fieldValue instanceof byte[]) {
-            return ((byte[]) fieldValue).length;
-        } else if (fieldValue instanceof char[]) {
-            return ((char[]) fieldValue).length;
-        } else if (fieldValue instanceof double[]) {
-            return ((double[]) fieldValue).length;
-        } else if (fieldValue instanceof float[]) {
-            return ((float[]) fieldValue).length;
-        } else if (fieldValue instanceof int[]) {
-            return ((int[]) fieldValue).length;
-        } else if (fieldValue instanceof long[]) {
-            return ((long[]) fieldValue).length;
-        } else if (fieldValue instanceof short[]) {
-            return ((short[]) fieldValue).length;
-        } else if (fieldValue == null) {
-            throw new RuntimeException("me.geso.tinyvalidator.rules.SizeConstraintValidator: The value is null");
-        } else {
-            throw new RuntimeException("me.geso.tinyvalidator.rules.SizeConstraintValidator can't get a size from: " + fieldValue.getClass());
-        }
-    }
+	private int getSize(Object fieldValue) {
+		if (fieldValue instanceof String) {
+			return ((String)fieldValue).codePointCount(0, ((String)fieldValue).length());
+		} else if (fieldValue instanceof CharSequence) {
+			return ((CharSequence)fieldValue).length();
+		} else if (fieldValue instanceof Collection) {
+			return ((Collection<?>)fieldValue).size();
+		} else if (fieldValue instanceof Map) {
+			return ((Map<?, ?>)fieldValue).size();
+		} else if (fieldValue instanceof Object[]) {
+			return ((Object[])fieldValue).length;
+		} else if (fieldValue instanceof boolean[]) {
+			return ((boolean[])fieldValue).length;
+		} else if (fieldValue instanceof byte[]) {
+			return ((byte[])fieldValue).length;
+		} else if (fieldValue instanceof char[]) {
+			return ((char[])fieldValue).length;
+		} else if (fieldValue instanceof double[]) {
+			return ((double[])fieldValue).length;
+		} else if (fieldValue instanceof float[]) {
+			return ((float[])fieldValue).length;
+		} else if (fieldValue instanceof int[]) {
+			return ((int[])fieldValue).length;
+		} else if (fieldValue instanceof long[]) {
+			return ((long[])fieldValue).length;
+		} else if (fieldValue instanceof short[]) {
+			return ((short[])fieldValue).length;
+		} else if (fieldValue == null) {
+			throw new RuntimeException("me.geso.tinyvalidator.rules.SizeConstraintValidator: The value is null");
+		} else {
+			throw new RuntimeException("me.geso.tinyvalidator.rules.SizeConstraintValidator can't get a size from: "
+				+ fieldValue.getClass());
+		}
+	}
 
 }
--- src/test/java/me/geso/tinyvalidator/rules/SizeConstraintValidatorTest.java
+++ src/test/java/me/geso/tinyvalidator/rules/SizeConstraintValidatorTest.java
@@ -1,13 +1,8 @@
 package me.geso.tinyvalidator.rules;
 
-import lombok.Data;
-import lombok.ToString;
-import me.geso.tinyvalidator.ConstraintViolation;
-import me.geso.tinyvalidator.Validator;
-import me.geso.tinyvalidator.constraints.Size;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -15,181 +10,197 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import lombok.Data;
+import lombok.ToString;
+import me.geso.tinyvalidator.ConstraintViolation;
+import me.geso.tinyvalidator.Validator;
+import me.geso.tinyvalidator.constraints.Size;
 
 public class SizeConstraintValidatorTest {
-    private static Logger logger = LoggerFactory.getLogger(SizeConstraintValidatorTest.class);
+	private static Logger logger = LoggerFactory.getLogger(SizeConstraintValidatorTest.class);
 
-    @Test
-    public void testTrueString() {
-        StringFoo stringFoo = new StringFoo();
-        stringFoo.setBar("hoge");
-        List<ConstraintViolation> violations = new Validator().validate(stringFoo);
-        assertTrue(violations.isEmpty());
-    }
+	@Test
+	public void testTrueString() {
+		StringFoo stringFoo = new StringFoo();
+		stringFoo.setBar("hoge");
+		List<ConstraintViolation> violations = new Validator().validate(stringFoo);
+		assertTrue(violations.isEmpty());
+	}
 
-    @Test
-    public void testFalseString() {
-        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "default");
-        logger.info("HHA");
+	@Test
+	public void testTrueStringSurrogate() {
+		StringFoo stringFoo = new StringFoo();
+		stringFoo.setBar("𠮷𠮷𠮷𠮷");
+		List<ConstraintViolation> violations = new Validator().validate(stringFoo);
+		assertTrue(violations.isEmpty());
+	}
 
-        StringFoo stringFoo = new StringFoo();
-        stringFoo.setBar("hogeeeee");
-        List<ConstraintViolation> violations = new Validator().validate(stringFoo);
-        String msg = violations.stream()
-                .map(violation -> violation.getName() + " " + violation.getMessage())
-                .collect(Collectors.joining(":::"));
-        assertEquals("bar size must be between 2 and 5", msg);
-        assertTrue(!violations.isEmpty());
-    }
+	@Test
+	public void testFalseString() {
+		System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "default");
+		logger.info("HHA");
 
-    @Test
-    public void testFalseStringTooShort() {
-        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "default");
+		StringFoo stringFoo = new StringFoo();
+		stringFoo.setBar("hogeeeee");
+		List<ConstraintViolation> violations = new Validator().validate(stringFoo);
+		String msg = violations.stream()
+			.map(violation -> violation.getName() + " " + violation.getMessage())
+			.collect(Collectors.joining(":::"));
+		assertEquals("bar size must be between 2 and 5", msg);
+		assertTrue(!violations.isEmpty());
+	}
 
-        StringFoo stringFoo = new StringFoo();
-        stringFoo.setBar("h");
-        List<ConstraintViolation> violations = new Validator().validate(stringFoo);
-        String msg = violations.stream()
-                .map(violation -> violation.getName() + " " + violation.getMessage())
-                .collect(Collectors.joining(":::"));
-        assertEquals("bar size must be between 2 and 5", msg);
-        assertTrue(!violations.isEmpty());
-    }
+	@Test
+	public void testFalseStringTooShort() {
+		System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "default");
 
-    @Data
-    @ToString
-    public static class StringFoo {
-        @Size(min = 2, max = 5)
-        private String bar;
+		StringFoo stringFoo = new StringFoo();
+		stringFoo.setBar("h");
+		List<ConstraintViolation> violations = new Validator().validate(stringFoo);
+		String msg = violations.stream()
+			.map(violation -> violation.getName() + " " + violation.getMessage())
+			.collect(Collectors.joining(":::"));
+		assertEquals("bar size must be between 2 and 5", msg);
+		assertTrue(!violations.isEmpty());
+	}
 
-    }
+	@Data
+	@ToString
+	public static class StringFoo {
+		@Size(min = 2, max = 5)
+		private String bar;
 
-    @Test
-    public void testSuccessCollection() {
-        CollectionFoo foo = new CollectionFoo();
-        foo.setBar(Arrays.asList("hoge", "fuga"));
-        List<ConstraintViolation> violations = new Validator().validate(foo);
-        assertTrue(violations.isEmpty());
-    }
+	}
 
-    @Test
-    public void testFailCollectionTooShort() {
-        CollectionFoo foo = new CollectionFoo();
-        foo.setBar(Arrays.asList());
-        List<ConstraintViolation> violations = new Validator().validate(foo);
-        assertFalse(violations.isEmpty());
-        String msg = violations.stream()
-                .map(violation -> violation.getName() + " " + violation.getMessage())
-                .collect(Collectors.joining(":::"));
-        assertEquals("bar size must be between 2 and 5", msg);
-    }
+	@Test
+	public void testSuccessCollection() {
+		CollectionFoo foo = new CollectionFoo();
+		foo.setBar(Arrays.asList("hoge", "fuga"));
+		List<ConstraintViolation> violations = new Validator().validate(foo);
+		assertTrue(violations.isEmpty());
+	}
 
-    @Test
-    public void testFailCollectionTooLarge() {
-        CollectionFoo foo = new CollectionFoo();
-        foo.setBar(Arrays.asList("a", "b", "c", "d", "e", "f"));
-        List<ConstraintViolation> violations = new Validator().validate(foo);
-        assertFalse(violations.isEmpty());
-        String msg = violations.stream()
-                .map(violation -> violation.getName() + " " + violation.getMessage())
-                .collect(Collectors.joining(":::"));
-        assertEquals("bar size must be between 2 and 5", msg);
-    }
+	@Test
+	public void testFailCollectionTooShort() {
+		CollectionFoo foo = new CollectionFoo();
+		foo.setBar(Arrays.asList());
+		List<ConstraintViolation> violations = new Validator().validate(foo);
+		assertFalse(violations.isEmpty());
+		String msg = violations.stream()
+			.map(violation -> violation.getName() + " " + violation.getMessage())
+			.collect(Collectors.joining(":::"));
+		assertEquals("bar size must be between 2 and 5", msg);
+	}
 
-    @Data
-    @ToString
-    public static class CollectionFoo {
-        @Size(min = 2, max = 5)
-        private List<String> bar;
+	@Test
+	public void testFailCollectionTooLarge() {
+		CollectionFoo foo = new CollectionFoo();
+		foo.setBar(Arrays.asList("a", "b", "c", "d", "e", "f"));
+		List<ConstraintViolation> violations = new Validator().validate(foo);
+		assertFalse(violations.isEmpty());
+		String msg = violations.stream()
+			.map(violation -> violation.getName() + " " + violation.getMessage())
+			.collect(Collectors.joining(":::"));
+		assertEquals("bar size must be between 2 and 5", msg);
+	}
 
-    }
+	@Data
+	@ToString
+	public static class CollectionFoo {
+		@Size(min = 2, max = 5)
+		private List<String> bar;
 
-    @Test
-    public void testSuccessMap() {
-        MapFoo foo = new MapFoo();
-        Map<String, String> map = new HashMap<>();
-        map.put("a", "1");
-        foo.setBar(map);
-        List<ConstraintViolation> violations = new Validator().validate(foo);
-        assertTrue(violations.isEmpty());
-    }
+	}
 
-    @Test
-    public void testFailMapTooSmall() {
-        MapFoo foo = new MapFoo();
-        Map<String, String> map = new HashMap<>();
-        foo.setBar(map);
-        List<ConstraintViolation> violations = new Validator().validate(foo);
-        assertFalse(violations.isEmpty());
-        String msg = violations.stream()
-                .map(violation -> violation.getName() + " " + violation.getMessage())
-                .collect(Collectors.joining(":::"));
-        assertEquals("bar size must be between 1 and 2", msg);
-    }
+	@Test
+	public void testSuccessMap() {
+		MapFoo foo = new MapFoo();
+		Map<String, String> map = new HashMap<>();
+		map.put("a", "1");
+		foo.setBar(map);
+		List<ConstraintViolation> violations = new Validator().validate(foo);
+		assertTrue(violations.isEmpty());
+	}
 
-    @Test
-    public void testFailMapTooLarge() {
-        MapFoo foo = new MapFoo();
-        Map<String, String> map = new HashMap<>();
-        map.put("a", "1");
-        map.put("b", "2");
-        map.put("c", "3");
-        foo.setBar(map);
-        List<ConstraintViolation> violations = new Validator().validate(foo);
-        assertFalse(violations.isEmpty());
-        String msg = violations.stream()
-                .map(violation -> violation.getName() + " " + violation.getMessage())
-                .collect(Collectors.joining(":::"));
-        assertEquals("bar size must be between 1 and 2", msg);
-    }
+	@Test
+	public void testFailMapTooSmall() {
+		MapFoo foo = new MapFoo();
+		Map<String, String> map = new HashMap<>();
+		foo.setBar(map);
+		List<ConstraintViolation> violations = new Validator().validate(foo);
+		assertFalse(violations.isEmpty());
+		String msg = violations.stream()
+			.map(violation -> violation.getName() + " " + violation.getMessage())
+			.collect(Collectors.joining(":::"));
+		assertEquals("bar size must be between 1 and 2", msg);
+	}
 
-    @Data
-    @ToString
-    public static class MapFoo {
-        @Size(min = 1, max = 2)
-        private Map<String, String> bar;
+	@Test
+	public void testFailMapTooLarge() {
+		MapFoo foo = new MapFoo();
+		Map<String, String> map = new HashMap<>();
+		map.put("a", "1");
+		map.put("b", "2");
+		map.put("c", "3");
+		foo.setBar(map);
+		List<ConstraintViolation> violations = new Validator().validate(foo);
+		assertFalse(violations.isEmpty());
+		String msg = violations.stream()
+			.map(violation -> violation.getName() + " " + violation.getMessage())
+			.collect(Collectors.joining(":::"));
+		assertEquals("bar size must be between 1 and 2", msg);
+	}
 
-    }
+	@Data
+	@ToString
+	public static class MapFoo {
+		@Size(min = 1, max = 2)
+		private Map<String, String> bar;
 
-    @Test
-    public void testSuccessArray() {
-        ArrayFoo foo = new ArrayFoo();
-        foo.setBar(new int[]{1, 2});
-        List<ConstraintViolation> violations = new Validator().validate(foo);
-        assertTrue(violations.isEmpty());
-    }
+	}
 
-    @Test
-    public void testFailArrayTooSmall() {
-        ArrayFoo foo = new ArrayFoo();
-        foo.setBar(new int[]{1});
-        List<ConstraintViolation> violations = new Validator().validate(foo);
-        assertFalse(violations.isEmpty());
-        String msg = violations.stream()
-                .map(violation -> violation.getName() + " " + violation.getMessage())
-                .collect(Collectors.joining(":::"));
-        assertEquals("bar size must be between 2 and 3", msg);
-    }
+	@Test
+	public void testSuccessArray() {
+		ArrayFoo foo = new ArrayFoo();
+		foo.setBar(new int[]{1, 2});
+		List<ConstraintViolation> violations = new Validator().validate(foo);
+		assertTrue(violations.isEmpty());
+	}
 
-    @Test
-    public void testFailArrayTooLarge() {
-        ArrayFoo foo = new ArrayFoo();
-        foo.setBar(new int[]{1, 2, 3, 4, 5});
-        List<ConstraintViolation> violations = new Validator().validate(foo);
-        assertFalse(violations.isEmpty());
-        String msg = violations.stream()
-                .map(violation -> violation.getName() + " " + violation.getMessage())
-                .collect(Collectors.joining(":::"));
-        assertEquals("bar size must be between 2 and 3", msg);
-    }
+	@Test
+	public void testFailArrayTooSmall() {
+		ArrayFoo foo = new ArrayFoo();
+		foo.setBar(new int[]{1});
+		List<ConstraintViolation> violations = new Validator().validate(foo);
+		assertFalse(violations.isEmpty());
+		String msg = violations.stream()
+			.map(violation -> violation.getName() + " " + violation.getMessage())
+			.collect(Collectors.joining(":::"));
+		assertEquals("bar size must be between 2 and 3", msg);
+	}
 
-    @Data
-    @ToString
-    public static class ArrayFoo {
-        @Size(min = 2, max = 3)
-        private int[] bar;
+	@Test
+	public void testFailArrayTooLarge() {
+		ArrayFoo foo = new ArrayFoo();
+		foo.setBar(new int[]{1, 2, 3, 4, 5});
+		List<ConstraintViolation> violations = new Validator().validate(foo);
+		assertFalse(violations.isEmpty());
+		String msg = violations.stream()
+			.map(violation -> violation.getName() + " " + violation.getMessage())
+			.collect(Collectors.joining(":::"));
+		assertEquals("bar size must be between 2 and 3", msg);
+	}
 
-    }
+	@Data
+	@ToString
+	public static class ArrayFoo {
+		@Size(min = 2, max = 3)
+		private int[] bar;
+
+	}
 
 }
